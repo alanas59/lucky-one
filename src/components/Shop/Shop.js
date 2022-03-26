641d1d07e7a403e3c21da4467eb253a61913a903
al-anas <alanas7171@gmail.com>
2022-03-26 20:04:00 +0600

    shop component updated

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,25 +1,40 @@
 import React, { useEffect, useState } from 'react';
-import Cart from '../Cart/Cart';
+
 import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
     const [products,setProducts] = useState([]);
+    const [cart,setCart] =useState([]);
+    const [chooseItem,setChooseItem] = useState("");
 
     useEffect(()=>{
          fetch('products.json')
          .then(res => res.json())
          .then(data => setProducts(data))
     },[]);
-    
+   
 
     const addToCart = product =>{
 
-        console.log(product);
+        const newCart = [...cart,product];
+        setCart(newCart);
+        
     }
 
+    const chooseOne = () =>{
+        const num = Math.ceil(Math.random() * 4);
+        setChooseItem(cart[num-1].name);
+     }
+
+    const chooseAgain = () =>{
+       setChooseItem("");
+       setCart([]);
+    }
+   
     return (
         <div className='shop-container'>
+         {/*    product section */}
             <div className="products-container">
                {
                  products.map(product => <Product 
@@ -29,8 +44,20 @@ const Shop = () => {
                     ></Product>)
                } 
             </div>
+
+           {/*  cart section */}
             <div className="cart-container">
-                <Cart></Cart>
+                <h2>Selected Products</h2>
+                {
+                  cart.map(cartItem => <li key = {cartItem.code}>{cartItem.name}</li>)
+                }
+                <h4>{chooseItem}</h4>
+                <button onClick={chooseOne}
+                className='choose-btn'
+                >Choose one for me</button>
+                <button onClick={chooseAgain}
+                className='choose-btn'
+                >Choose again</button>
             </div>
         </div>
     );
