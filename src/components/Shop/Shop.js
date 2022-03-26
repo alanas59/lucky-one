a43037ff03522b0c1bec445c5846990f1eed8814
al-anas <alanas7171@gmail.com>
2022-03-26 15:20:51 +0600

    shop container added

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Shop.css';
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                 
+            </div>
+            <div className="cart-container">
+                cart
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
